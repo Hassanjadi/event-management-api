0c39ef110d8b71009a063125973fcd38e6ad0b16
Hassanjadi <hassanjadi25@gmail.com>
2024-10-21 01:36:43 +0700

    fetat: basic route

--- main.go
+++ main.go
@@ -45,6 +45,12 @@ func main() {
     router := gin.Default()
     router.Use(middleware.Auth())
 
+    router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Event Management System Api - Happy Coding!",
+		})
+	})
+
     router.GET("/api/users", controllers.GetAllUser)
     router.POST("/api/users", controllers.InsertUser)
     router.GET("/api/users/:id", controllers.GetUser)
